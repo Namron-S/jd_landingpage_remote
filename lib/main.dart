752e82f9b048c9088a7787edfd533d832f237fe6
Namron-S <N.Sutatyo@gmail.com>
2021-01-20 17:19:40 +0100

    text section: 2. draft: added long dummy strings

--- lib/main.dart
+++ lib/main.dart
@@ -89,8 +89,18 @@ class MyHomePage extends StatelessWidget {
             fallbackWidth: size.width / 3,
             fallbackHeight: size.width / 3,
           ),
-          Column(
-            children: [Text('Subway Deventer Centrum'), Text('Lorem Ipsum')],
+          Flexible(
+            child: Column(
+              children: [
+                Text('Subway Deventer Centrum'),
+                Text(
+                  constStrLoremIpsum,
+                ),
+                Text(
+                  constStrNullamDictum,
+                )
+              ],
+            ),
           )
         ],
       ),
@@ -120,8 +130,9 @@ class MyHomePage extends StatelessWidget {
   }
 
 //Helper Constants:
-  static const String loremIpsumStr =
+//normally these Strings are retrieved by a function, but for the case of simplicity we have constants.
+  static const String constStrLoremIpsum =
       'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.';
-  static const String nullamDictumStr =
+  static const String constStrNullamDictum =
       'Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.';
 }
