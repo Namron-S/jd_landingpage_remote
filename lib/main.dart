73ce8ee88d3aab094ade9848e56e22da7d7d0822
Namron-S <N.Sutatyo@gmail.com>
2021-01-20 13:36:36 +0100

    mainStructure

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Subway Landingpage',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,9 +28,7 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Subway'),
       ),
-      body: Container(
-        child: FlutterLogo(),
-      ),
+      body: ListView(children: [FlutterLogo(), FlutterLogo(), FlutterLogo()]),
     );
   }
 }
