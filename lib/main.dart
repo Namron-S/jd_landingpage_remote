d62fa3ba15aa5657f66ddf2521240276d2585213
Namron-S <N.Sutatyo@gmail.com>
2021-01-20 15:26:39 +0100

    title section: 2. draft: alignment of elements

--- lib/main.dart
+++ lib/main.dart
@@ -24,29 +24,50 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const iconSize = 12.0;
+    const iconSize = 15.0;
     const fontSize = 8.0;
+
     Widget titleSection = Container(
         child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
       children: [
         FlutterLogo(),
-        Icon(Icons.location_on, color: Colors.green, size: iconSize),
-        Text(
-          'Subway Deventer Centrum | Smedenstraat 14.',
-          style: TextStyle(fontSize: fontSize),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.location_on, color: Colors.green, size: iconSize),
+            Text(
+              'Subway Deventer Centrum | Smedenstraat 14.',
+              style: TextStyle(fontSize: fontSize),
+            ),
+          ],
         ),
-        Icon(
-          Icons.phone,
-          color: Colors.green,
-          size: iconSize,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.phone,
+              color: Colors.green,
+              size: iconSize,
+            ),
+            Text(
+              '0570 745 108',
+              style: TextStyle(fontSize: fontSize),
+            ),
+          ],
         ),
-        Text(
-          '0570 745 108',
-          style: TextStyle(fontSize: fontSize),
-        ),
-        Icon(Icons.account_circle_outlined,
-            color: Colors.green, size: iconSize),
-        Icon(Icons.menu, color: Colors.green)
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.account_circle_outlined,
+                color: Colors.green, size: iconSize),
+            Icon(Icons.menu, color: Colors.green, size: iconSize),
+          ],
+        )
       ],
     ));
 
