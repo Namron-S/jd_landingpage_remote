6873e5caf911974f92bebcf84ba41b99565db493
Namron-S <N.Sutatyo@gmail.com>
2021-02-16 15:21:04 +0100

    implemented "showAlertDialog"

--- lib/main.dart
+++ lib/main.dart
@@ -289,3 +289,23 @@ class MyHomePage extends StatelessWidget {
   static const String constStrNullamDictum =
       'Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.';
 }
+
+Future<void> showAlertDialog(BuildContext context, String msg) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Info'),
+        content: Text(msg),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Ok'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
